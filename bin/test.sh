edd473c7b663870225d6d1b8c84c63dcb0e7b23b
si-eci <i.ishiai@sint.co.jp>
2014-12-22 16:14:13 +0900

    are

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
+SECURITY_GROUP_NAME="circleci"
+
 IP_IO=`curl -s ifconfig.io`
 
 echo $IP_IO
 
-aws ec2 describe-security-groups --group-name "circleci"
+aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME}
