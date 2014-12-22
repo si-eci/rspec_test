ab31b50a833bcf34d98dfe38378b54f1b050623d
si-eci <i.ishiai@sint.co.jp>
2014-12-22 17:25:08 +0900

    are

--- bin/test.sh
+++ bin/test.sh
@@ -8,16 +8,11 @@ IP_IO=`curl -s ifconfig.io`
 
 echo $IP_IO
 
-aws --version
-
-aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME}
-aws ec2 describe-security-groups --group-names "circleci"
-
 if aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME} | grep ${IP_IO} > /dev/null
 then
-  echo 'ari'
+  echo 'exists'
 else
-  echo 'nasi'
+  echo 'not exists'
   aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 3306 --cidr ${IP_IO}/32
   aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 7700 --cidr ${IP_IO}/32
 fi
