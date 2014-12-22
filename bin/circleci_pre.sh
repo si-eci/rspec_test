6aa6c4733dc1bfcd1e1ba2cbf9872727cf27d48e
si-eci <i.ishiai@sint.co.jp>
2014-12-22 15:12:53 +0900

    are

---
+++ bin/circleci_pre.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
+SECURITY_GROUP_NAME="circleci"
+IP=`curl -s ifconfig.me`
+
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 3306 --cidr ${IP}/32
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 7700 --cidr ${IP}/32
