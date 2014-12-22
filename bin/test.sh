f9a36a41287b23720bb6217949ca166b5548da70
si-eci <i.ishiai@sint.co.jp>
2014-12-22 17:01:21 +0900

    are

--- bin/test.sh
+++ bin/test.sh
@@ -8,7 +8,10 @@ IP_IO=`curl -s ifconfig.io`
 
 echo $IP_IO
 
+aws --version
+
 aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME}
+aws ec2 describe-security-groups --group-names "circleci"
 
 if aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME} | grep ${IP_IO} > /dev/null
 then
