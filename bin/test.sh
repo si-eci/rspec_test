08ba17c9680bc9534b1c0c43f902c46817381e41
si-eci <i.ishiai@sint.co.jp>
2014-12-22 17:13:02 +0900

    are

--- bin/test.sh
+++ bin/test.sh
@@ -15,7 +15,9 @@ aws ec2 describe-security-groups --group-names "circleci"
 
 if aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME} | grep ${IP_IO} > /dev/null
 then
-   echo 'ari'
+  echo 'ari'
 else
-   echo 'nasi'
+  echo 'nasi'
+  aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 3306 --cidr ${IP_IO}/32
+  aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 7700 --cidr ${IP_IO}/32
 fi
