e33673acd9ecdf1f5336ae90fa026232f61f5e20
si-eci <i.ishiai@sint.co.jp>
2015-05-15 11:59:06 +0900

    are

--- spec/login.rb
+++ spec/login.rb
@@ -4,5 +4,11 @@ driver = Selenium::WebDriver.for :firefox
 
 driver.navigate.to 'http://devtest2.socs-staging.center/'
 
+element = driver.find_element(:id, 'user_login')
+element.send_keys('socsuser')
+
+driver.find_element(:id, 'user_password').send_keys('si2015devtest2')
+
+driver.find_element(:name, 'button').click
 
 driver.quit
