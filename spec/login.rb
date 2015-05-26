bffb9989bf3ba37bb07a2fd7e9522d4dc0ed4f97
si-eci <i.ishiai@sint.co.jp>
2015-05-26 16:13:21 +0900

    are

--- spec/login.rb
+++ spec/login.rb
@@ -28,8 +28,6 @@ describe "SOCS" do
     click_button "ログイン"
     
     expect(page).to have_text('ユーザーメイン画面')
-    #expect(page).to have_text('m9(^Д^)ﾌﾟｷﾞｬｰ')
-
   end 
 
 end 
@@ -51,10 +49,7 @@ describe "商品関連" do
   end
   
   it "商品カテゴリ管理に遷移できること" do
-    visit 'http://devtest2.socs-staging.center/'
-    
     click_link "商品カテゴリ管理"
-    
     expect(page).to have_text('商品カテゴリ管理')
     expect(current_path).to eq "/admin/item_categories"
   end
@@ -64,13 +59,37 @@ describe "商品関連" do
     expect(page).to have_text('規格管理')
     expect(current_path).to eq "/admin/standards"
   end
-
-
-
-
-
-
-
+  
+  it "タグ管理に遷移できること" do
+    click_link "タグ管理"
+    expect(page).to have_text('タグ管理')
+    expect(current_path).to eq "/admin/tags"
+  end
+  
+  it "ブランド管理に遷移できること" do
+    click_link "ブランド管理"
+    expect(page).to have_text('ブランド管理')
+    expect(current_path).to eq "/admin/brands"
+  end
+  
+  it "ギフト管理に遷移できること" do
+    click_link "ギフト管理"
+    expect(page).to have_text('ギフト管理')
+    expect(current_path).to eq "/admin/gifts"
+  end
+  
+  it "商品分類1管理" do
+    click_link "商品分類1管理"
+    expect(page).to have_text('商品分類1管理')
+    expect(current_path).to eq "/admin/item1_categories"
+  end
+  
+  it "商品分類2管理" do
+    click_link "商品分類2管理"
+    expect(page).to have_text('商品分類2管理')
+    expect(current_path).to eq "/admin/item2_categories"
+  end
+  
 end
 
 
