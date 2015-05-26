f990c01f231f5b2966c3ccd0de1aa0dcdd3dbcce
si-eci <i.ishiai@sint.co.jp>
2015-05-26 12:18:53 +0900

    are

--- spec/login.rb
+++ spec/login.rb
@@ -1,14 +1,34 @@
 require 'selenium-webdriver'
+require 'capybara/rspec'
 
-driver = Selenium::WebDriver.for :firefox
+# RSpecにCapybaraのDSLを設定
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
 
-driver.navigate.to 'http://devtest2.socs-staging.center/'
 
-element = driver.find_element(:id, 'user_login')
-element.send_keys('socsuser')
+# Capybaraにseleniumを使うように設定
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://devtest2.socs-staging.center/'
 
-driver.find_element(:id, 'user_password').send_keys('si2015devtest2')
 
-driver.find_element(:name, 'button').click
+describe "SOCS" do
 
-driver.quit
+  before do
+    visit '/'
+  end
+
+  if "トップページが表示されること" do
+    expect(page).to have_content('user_login')
+  end
+
+end
+
+
+#driver = Selenium::WebDriver.for :firefox
+#driver.navigate.to 'http://devtest2.socs-staging.center/'
+#element = driver.find_element(:id, 'user_login')
+#element.send_keys('socsuser')
+#driver.find_element(:id, 'user_password').send_keys('si2015devtest2')
+#driver.find_element(:name, 'button').click
+#driver.quit
