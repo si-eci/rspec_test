aec52d0d5363ab96f0f85806b3957ff81c57c939
si-eci <i.ishiai@sint.co.jp>
2015-05-26 14:33:46 +0900

    are

--- spec/login.rb
+++ spec/login.rb
@@ -18,19 +18,28 @@ describe "SOCS" do
     visit 'http://devtest2.socs-staging.center/'
   end
 
-  #it "トップページが表示されること" do
-  #  expect(page).to have_content("ログイン")
-  #end
+  it "トップページが表示されること" do
+    expect(page).to have_text("ログイン画面")
+  end
   
   it "ログインできること" do
     fill_in 'user_login', with: 'socsuser'
     fill_in 'user_password', with: 'si2015devtest2'
     click_button "ログイン"
     
-    #expect(page).to have_text('ユーザーメイン画面')
-    expect(page).to have_text('m9(^Д^)ﾌﾟｷﾞｬｰ')
-    
+    expect(page).to have_text('ユーザーメイン画面')
+    #expect(page).to have_text('m9(^Д^)ﾌﾟｷﾞｬｰ')
   end 
+
+
+  it "商品管理に遷移できること" do
+    click_link "商品管理"
+    
+    expect(page).to have_text('商品管理')
+    expect(current_path).to eq "/admin/items"
+  end
+
+
 
 end
 
