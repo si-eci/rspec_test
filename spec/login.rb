0e43afdbe813ef076ccb6b73bde3a225e689bb19
si-eci <i.ishiai@sint.co.jp>
2015-05-26 16:23:09 +0900

    are

--- spec/login.rb
+++ spec/login.rb
@@ -33,65 +33,6 @@ describe "SOCS" do
 end 
 
 
-describe "商品関連" do
-  
-  before do
-    visit 'http://devtest2.socs-staging.center/'
-    fill_in 'user_login', with: 'socsuser'
-    fill_in 'user_password', with: 'si2015devtest2'
-    click_button "ログイン"
-  end
-  
-  it "商品管理に遷移できること" do
-    click_link "商品管理"
-    expect(page).to have_text('商品管理')
-    expect(current_path).to eq "/admin/items"
-  end
-  
-  it "商品カテゴリ管理に遷移できること" do
-    click_link "商品カテゴリ管理"
-    expect(page).to have_text('商品カテゴリ管理')
-    expect(current_path).to eq "/admin/item_categories"
-  end
-  
-  it "規格管理に遷移できること" do
-    click_link "規格管理"
-    expect(page).to have_text('規格管理')
-    expect(current_path).to eq "/admin/standards"
-  end
-  
-  it "タグ管理に遷移できること" do
-    click_link "タグ管理"
-    expect(page).to have_text('タグ管理')
-    expect(current_path).to eq "/admin/tags"
-  end
-  
-  it "ブランド管理に遷移できること" do
-    click_link "ブランド管理"
-    expect(page).to have_text('ブランド管理')
-    expect(current_path).to eq "/admin/brands"
-  end
-  
-  it "ギフト管理に遷移できること" do
-    click_link "ギフト管理"
-    expect(page).to have_text('ギフト管理')
-    expect(current_path).to eq "/admin/gifts"
-  end
-  
-  it "商品分類1管理" do
-    click_link "商品分類1管理"
-    expect(page).to have_text('商品分類1管理')
-    expect(current_path).to eq "/admin/item1_categories"
-  end
-  
-  it "商品分類2管理" do
-    click_link "商品分類2管理"
-    expect(page).to have_text('商品分類2管理')
-    expect(current_path).to eq "/admin/item2_categories"
-  end
-  
-end
-
 
 #driver = Selenium::WebDriver.for :firefox
 #driver.navigate.to 'http://devtest2.socs-staging.center/'
